93bfe711f532518fc2326a9f36339a3b7c35a094
Simone Taeggi <startae14@gmail.com>
2023-04-04 11:25:00 +0200

    Define the Posts class with all the methods involved

---
+++ src/posts/index.ts
@@ -0,0 +1,21 @@
+import { Base } from "src/base";
+import { Post, NewPost } from "./types";
+
+const resource = 'posts';
+
+export class Posts extends Base {
+  getPostById(id: number): Promise<Post> {
+    return this.invoke(`/${resource}/${id}`);
+  }
+
+  getPosts(): Promise<Post[]> {
+    return this.invoke(`/${resource}`);
+  }
+
+  createPost(newPost: NewPost): Promise<Post> {
+    return this.invoke(`/${resource}`, {
+      method: 'POST',
+      body: JSON.stringify(newPost),
+    });
+  }
+}
